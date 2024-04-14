8261cf2e34c7c71149c48cfbd92b5c7cb9d69677
Serdar Emir BOSTANCI <95711128+SerdarEmirBOSTANCI@users.noreply.github.com>
2024-04-14 23:50:19 +0300

    proje tekrar oluşturuluyor
    projeyi hatalardan dolayı baştan yüklüyorum kodları tekrar yazıyorum.

--- sahafimMobil/App.js
+++ sahafimMobil/App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+const AnaSayfa = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.baslik}>Sahafım</Text>
+      <View style={styles.butonlarContainer}>
+        <TouchableOpacity style={styles.buton}>
+          <Text style={styles.butonMetin}>Kitaplık</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buton}>
+          <Text style={styles.butonMetin}>Kitap Ekle</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.butonlarContainer}>
+        <TouchableOpacity style={styles.buton}>
+          <Text style={styles.butonMetin}>Günlük Kazanç</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buton}>
+          <Text style={styles.butonMetin}>Aylık Kazanç</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.butonlarContainer}>
+        <TouchableOpacity style={styles.buton}>
+          <Text style={styles.butonMetin}>Profil</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  baslik: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    fontFamily: 'font1',
+    marginBottom: 20,
+  },
+  butonlarContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  buton: {
+    width: 150,
+    height: 150,
+    backgroundColor: 'black',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10, // Butonların arasına sağ taraftan 10 birimlik boşluk eklendi
+  },
+  butonMetin: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
+
+export default AnaSayfa;
